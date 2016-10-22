c905cd234ef8e5178c1180ba1e9ea7b4258319b9
Cheryl Spanos <chspanos@comcast.net>
2016-10-21 23:04:09 -0700

    feat: Implement multiple cats
    To add more cats to the application:
    1. Create cat array by copying data from cat-clicker-pro
    2. Create catList as a ko observable array in ViewModel & load with
       cat data
    3. Change call to currentCat to reference the first cat in catList
    4. Make list in HTML to display list of cats. Use ko data bindings to
    	automatically populate the list items
    5. Create a changeCat function in ViewModel and use ko data bindings to
    	call the function when you click on a cat in the list. The changeCat
    	function changes currentCat to the selected cat.

--- js/app.js
+++ js/app.js
@@ -1,10 +1,47 @@
 // Model section
-var Cat = function() {
-  this.clickCount = ko.observable(0);
-  this.name = ko.observable('Ginger');
-  this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
-  this.imgAttribution = ko.observable('Attribution unknown');
-  this.nicknames = ko.observableArray(['Gigi', 'Cuddles', 'Foofles']);
+var initialCats = [{
+    clickCount: 0,
+    name: 'Ginger',
+    imgSrc: 'img/434164568_fea0ad4013_z.jpg',
+    imgAttribution: 'https://www.flickr.com/photos/bigtallguy/434164568',
+    nicknames: ['Gigi']
+  },
+  {
+    clickCount: 0,
+    name: 'Butterscotch',
+    imgSrc: 'http://fixnation.org/wordpress/wp-content/uploads/2014/03/cats-kittens_00379052.jpg',
+    imgAttribution: 'http://fixnation.org/wordpress/wp-content/uploads/2014/03/cats-kittens_00379052',
+    nicknames: ['B-ball']
+  },
+  {
+    clickCount: 0,
+    name: 'Felix',
+    imgSrc: 'http://wondrouspics.com/wp-content/uploads/2011/12/Cute-Kitten2.jpg',
+    imgAttribution: 'http://wondrouspics.com/wp-content/uploads/2011/12/Cute-Kitten2',
+    nicknames: ['Tiger']
+  },
+  {
+    clickCount: 0,
+    name: 'Snowball',
+    imgSrc: 'http://images2.fanpop.com/image/photos/9100000/kitty-kitties-9109284-500-460.jpg',
+    imgAttribution: 'http://images2.fanpop.com/image/photos/9100000/kitty-kitties-9109284',
+    nicknames: ['Sweetpea']
+  },
+  {
+    clickCount: 0,
+    name: 'Tucker',
+    imgSrc: 'http://images.fanpop.com/images/image_uploads/Sleepy-Kitty-cats-153784_724_543.jpg',
+    imgAttribution: 'http://images.fanpop.com/images/image_uploads/Sleepy-Kitty-cats-153784',
+    nicknames: ['Sleepyhead']
+  }
+];
+
+var Cat = function(data) {
+  this.clickCount = ko.observable(data.clickCount);
+  this.name = ko.observable(data.name);
+  this.imgSrc = ko.observable(data.imgSrc);
+  this.imgAttribution = ko.observable(data.imgAttribution);
+  this.nicknames = ko.observableArray(data.nicknames);
 
   this.level = ko.computed(function() {
     var count = this.clickCount();
@@ -28,7 +65,17 @@ var Cat = function() {
 var ViewModel = function() {
   var self = this;
 
-  this.currentCat = ko.observable(new Cat());
+  this.catList = ko.observableArray([]);
+
+  initialCats.forEach(function(catItem){
+    self.catList.push(new Cat(catItem));
+  });
+
+  this.currentCat = ko.observable( this.catList()[0] );
+
+  this.changeCat = function( selectedCat ) {
+    self.currentCat(selectedCat);
+  };
 
   this.incrementCounter = function() {
     self.currentCat().clickCount(self.currentCat().clickCount() + 1);
